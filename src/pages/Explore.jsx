c9f5d10e93b046c91c7961edaae3d2e8bfb57d89
Pedro Henrique <pedrohrds05@hotmail.com>
2022-01-10 14:23:04 -0300

    Adicionando estilo pagina de explorar

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -2,16 +2,31 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import { container, buttonsExplore, explore } from '../styles/Explore.module.css';
 
 const Explore = () => (
-  <div>
+  <div className={ container }>
     <Header />
-    <Link to="/explorar/comidas">
-      <button data-testid="explore-food" type="button">Explorar Comidas</button>
-    </Link>
-    <Link to="/explorar/bebidas">
-      <button data-testid="explore-drinks" type="button">Explorar Bebidas</button>
-    </Link>
+    <section className={ explore }>
+      <Link to="/explorar/comidas">
+        <button
+          className={ buttonsExplore }
+          data-testid="explore-food"
+          type="button"
+        >
+          Explorar Comidas
+        </button>
+      </Link>
+      <Link to="/explorar/bebidas">
+        <button
+          className={ buttonsExplore }
+          data-testid="explore-drinks"
+          type="button"
+        >
+          Explorar Bebidas
+        </button>
+      </Link>
+    </section>
     <Footer />
   </div>
 );
--- src/pages/ExploreDrinks.jsx
+++ src/pages/ExploreDrinks.jsx
@@ -3,6 +3,9 @@ import { Link } from 'react-router-dom';
 import RecipeContext from '../context/RecipeContext';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import {
+  container, supreseMe, containerDrink, buttonsExplore,
+} from '../styles/Explore.module.css';
 
 const ExploreDrinks = () => {
   const { drinks, handleDrinks } = useContext(RecipeContext);
@@ -12,22 +15,31 @@ const ExploreDrinks = () => {
   });
 
   return (
-    <>
+    <div className={ container }>
       <Header />
-      <Link to="/explorar/bebidas/ingredientes">
-        <button
-          type="button"
-          data-testid="explore-by-ingredient"
-        >
-          Por Ingredientes
+      <section className={ containerDrink }>
+        <Link to="/explorar/bebidas/ingredientes">
+          <button
+            className={ buttonsExplore }
+            type="button"
+            data-testid="explore-by-ingredient"
+          >
+            Por Ingredientes
 
-        </button>
-      </Link>
-      <Link to={ drinks.length ? `/bebidas/${drinks[0].idDrink}` : '' }>
-        <button type="button" data-testid="explore-surprise">Me Surpreenda!</button>
-      </Link>
+          </button>
+        </Link>
+        <Link to={ drinks.length ? `/bebidas/${drinks[0].idDrink}` : '' }>
+          <button
+            className={ supreseMe }
+            type="button"
+            data-testid="explore-surprise"
+          >
+            Me Surpreenda!
+          </button>
+        </Link>
+      </section>
       <Footer />
-    </>
+    </div>
   );
 };
 
